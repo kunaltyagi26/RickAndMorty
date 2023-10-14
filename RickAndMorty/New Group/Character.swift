04aef9d9e444872e68529dc528e523bdbddaf737
kunaltyagi26 <kunaltyagi32@gmail.com>
2023-10-14 20:15:31 +0530

    Added models.

--- RickAndMorty/New Group/Character.swift
+++ RickAndMorty/New Group/Character.swift
@@ -8,5 +8,34 @@
 import Foundation
 
 struct Character: Decodable {
-    
+    let id: Int
+    let name: String
+    let status: CharacterStatus
+    let species: String
+    let type: String
+    let gender: CharacterGender
+    let origin: Object
+    let location: Object
+    let image: String
+    let episode: [String]
+    let url: String
+    let created: String
+}
+
+struct Object: Decodable {
+    let name: String
+    let url: String
+}
+
+enum CharacterStatus: String, Decodable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
+
+enum CharacterGender: String, Decodable {
+    case female = "Female"
+    case male = "Male"
+    case genderless = "Genderless"
+    case unknown = "unknown"
 }
--- RickAndMorty/New Group/Episode.swift
+++ RickAndMorty/New Group/Episode.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 struct Episode: Decodable {
+    let id: Int
+    let name: String
+    let airDate: String
+    let episode: String
+    let characters: [String]
+    let url: String
+    let created: String
     
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case airDate = "air_date"
+        case episode
+        case characters
+        case url
+        case created
+    }
 }
