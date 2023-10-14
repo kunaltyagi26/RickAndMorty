4a509b7989ab76288c950e88655594fe0b17ff5f
kunaltyagi26 <kunaltyagi32@gmail.com>
2023-10-14 18:44:46 +0530

    Setup project.

--- RickAndMorty/New Group/Character.swift
+++ RickAndMorty/New Group/Character.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct Character: Decodable {
+    
+}
--- RickAndMorty/New Group/Core/TabViewController.swift
+++ RickAndMorty/New Group/Core/TabViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class TabViewController: UITabBarController {
+final class TabViewController: UITabBarController {
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setupTabs()
     }
 
-
+    private func setupTabs() {
+        let charactersVC = CharacterViewController()
+        let locationVC = LocationViewController()
+        let episodeVC = EpisodeViewController()
+        let settingsVC = SettingsViewController()
+        
+        for viewController in [charactersVC, locationVC, episodeVC, settingsVC] {
+            viewController.navigationItem.largeTitleDisplayMode = .automatic
+        }
+        
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav2 = UINavigationController(rootViewController: locationVC)
+        let nav3 = UINavigationController(rootViewController: episodeVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        nav1.tabBarItem = UITabBarItem(
+            title: "Characters",
+            image: UIImage(systemName: "person"),
+            tag: 0
+        )
+        nav2.tabBarItem = UITabBarItem(
+            title: "Location",
+            image: UIImage(systemName: "globe"),
+            tag: 1
+        )
+        nav3.tabBarItem = UITabBarItem(
+            title: "Episodes",
+            image: UIImage(systemName: "tv"),
+            tag: 2
+        )
+        nav4.tabBarItem = UITabBarItem(
+            title: "Settings",
+            image: UIImage(systemName: "gear"),
+            tag: 3
+        )
+        
+        for navigationController in [nav1, nav2, nav3, nav4] {
+            navigationController.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers(
+            [nav1, nav2, nav3, nav4],
+            animated: true
+        )
+    }
 }
 
